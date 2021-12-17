ec96002a5002f4fced387d6e6a7b262f563f001a
Roynul Rohan <roynulrohan@gmail.com>
2021-12-17 03:01:56 -0500

    Refactored directory structure for server

--- client/src/pages/MarketPage.tsx
+++ client/src/pages/MarketPage.tsx
@@ -46,7 +46,6 @@ export default function MarketPage() {
         <div className='dark:bg-darkBg h-full w-full min-h-screen flex flex-col items-center text-center'>
             <div className='mt-20 text-3xl font-medium dark:text-white'>Browse the market.</div>
             <div className='mt-5 text-md px-5 md:text-xl text-gray-600 dark:text-gray-400'>Explore our selection of the biggest names in the industry.</div>
-            <div className='mt-5 text-xs px-5 md:text-sm text-red-400 dark:text-red-400'>* All data is fake and do not represent real stocks</div>
             <div className='mt-20 w-full flex flex-col'>
                 <div className='px-8 sm:px-12 md:px-14 lg:px-20 xl:px-60'>
                     <input
@@ -54,7 +53,7 @@ export default function MarketPage() {
                         id='market-search-input'
                         spellCheck='false'
                         className='w-full text-sm sm:text-base h-16 rounded-lg border-transparent flex-2 appearance-none py-2 px-10 border-0 bg-white dark:bg-darkField text-gray-700 dark:text-gray-200 placeholder-gray-400 shadow-md focus:outline-none focus:ring-2 dark:focus:ring-gray-700 focus:border-transparent'
-                        placeholder='Search by ticker or company'
+                        placeholder='Search by ticker, company, description'
                         value={searchQuery}
                         onChange={searchOnChange}
                     />
