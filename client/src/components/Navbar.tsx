03f5451ed2bafb23d089c35e8fb2d996c562dc23
Roynul Rohan <roynulrohan@gmail.com>
2024-01-20 20:43:10 -0500

    Refactor Navbar menu styling

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -164,7 +164,7 @@ export default function NavBar() {
                                                         leave='transition ease-in duration-75'
                                                         leaveFrom='transform opacity-100 scale-100'
                                                         leaveTo='transform opacity-0 scale-95'>
-                                                        <Menu.Items className='origin-top-right absolute right-0 mt-2 w-48 rounded-2xl shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                                                        <Menu.Items className='origin-top-right absolute right-0 mt-2 w-48 rounded-2xl shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
                                                             <Menu.Item>
                                                                 {({ active }: any) => (
                                                                     <Link
