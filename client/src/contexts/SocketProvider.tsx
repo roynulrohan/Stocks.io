fd7e4127c55909c2e2380a8f0fc1386af3f2dedb
Roynul Rohan <roynulrohan@gmail.com>
2024-01-22 03:48:57 -0500

    Update SocketProvider to use VITE_SOCKET_PATH

--- client/src/contexts/SocketProvider.tsx
+++ client/src/contexts/SocketProvider.tsx
@@ -8,7 +8,7 @@ interface Params {
 }
 
 export function SocketProvider({ children }: Params) {
-    console.log(import.meta.env.VITE_API_URI, window.location.origin);
+    console.log(import.meta.env.VITE_API_URI, import.meta.env.VITE_SOCKET_PATH);
     const socket: Socket = io(import.meta.env.VITE_API_URI || window.location.origin, {
         timeout: 10001,
         transports: ['websocket'],
